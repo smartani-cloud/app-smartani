6d4fab40d084142630dc5ab3d30613e2a9fa4b3a
Digiyok <admin@digiyok.com>
2025-07-09 09:33:05 +0700

    removing head 2

--- Modules/Access/app/Services/RoleRelationResolverImpl.php
+++ Modules/Access/app/Services/RoleRelationResolverImpl.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace Modules\Access\Services;
@@ -12,19 +11,5 @@ class RoleRelationResolverImpl implements RoleRelationResolver
     {
         return $user->belongsTo('Modules\Access\Models\Role', 'role_id');
     }
-=======
-<?php
 
-namespace Modules\Access\Services;
-
-use App\Models\User;
-use Modules\Access\Contracts\RoleRelationResolver;
-
-class RoleRelationResolverImpl implements RoleRelationResolver
-{
-    public function resolve(User $user)
-    {
-        return $user->belongsTo('Modules\Access\Models\Role', 'role_id');
-    }
->>>>>>> 519c7866245bb7df43bd5924d819bc4ab649e1f7
 }
