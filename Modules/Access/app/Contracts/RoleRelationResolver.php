be764aca4e2946b558372f558d80a9e6faac9c2d
Digiyok <admin@digiyok.com>
2025-07-09 09:34:26 +0700

    Update RoleRelationResolver.php

--- Modules/Access/app/Contracts/RoleRelationResolver.php
+++ Modules/Access/app/Contracts/RoleRelationResolver.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace Modules\Access\Contracts;
@@ -8,15 +7,4 @@ use App\Models\User;
 interface RoleRelationResolver
 {
     public function resolve(User $user);
-=======
-<?php
-
-namespace Modules\Access\Contracts;
-
-use App\Models\User;
-
-interface RoleRelationResolver
-{
-    public function resolve(User $user);
->>>>>>> 519c7866245bb7df43bd5924d819bc4ab649e1f7
 }
